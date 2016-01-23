2ae44f9910530d06be342a257b0a617d28bfb7dc
6pi42 <cyprien-33@hotmail.fr>
2016-01-23 21:57:26 +0100

    MAJ fractol.h

--- fractol/fractol.h
+++ fractol/fractol.h
@@ -18,6 +18,7 @@
 # define HEIGHT 1000
 # define WIDTH 1000
 # define KEY_ESC 53
+# define MAX_DEPTH 8
 
 typedef struct		s_env
 {
@@ -34,11 +35,28 @@ typedef struct		s_img
 	void			*img;
 }					t_img;
 
+typedef struct		s_point
+{
+	int				x;
+	int				y;
+}					t_point;
+
 typedef struct		s_comp
 {
 	double			r;
 	double			i;
 }					t_comp;
+
+typedef struct		s_bres
+{
+	int				dx;
+	int				dy;
+	int				e;
+	int				sx;
+	int				sy;
+	int				inc1;
+	int				inc2;
+}					t_bres;
 
 typedef struct		s_map
 {
@@ -51,10 +69,14 @@ typedef struct		s_map
 	t_comp			julia;
 }					t_map;
 
+void				ft_print_choices(void);
 void				ft_error(void);
 void				pixel_put(t_map *map, int x, int y, int color);
 void				init_img(t_map *map, int color);
 void				mandelbrot(t_map *map, int max_iter);
 void				draw(t_map *map);
+void				julia(t_map *map, int max_iter);
+void				draw_line(t_point pt1, t_point pt2, t_map *map);
+void				sierpinski(t_map *map);
 
 #endif

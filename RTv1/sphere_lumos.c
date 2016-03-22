6e47e66f51552ebac952718b230905a6796460ed
6pi42 <cboyer@student.42.fr>
2016-03-22 17:04:06 +0100

    specular...

--- RTv1/sphere_lumos.c
+++ RTv1/sphere_lumos.c
@@ -6,7 +6,7 @@
 /*   By: cboyer <cboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/03 13:31:54 by cboyer            #+#    #+#             */
-/*   Updated: 2016/03/22 15:07:19 by cboyer           ###   ########.fr       */
+/*   Updated: 2016/03/22 15:24:04 by cboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ t_vec	get_normal_sphere(t_sphere *sphere, t_vec ray, t_map *map)
 {
 	t_vec norm;
 
-	norm = sous_vec(sphere->pos, intersection(sphere, ray, map->tab->cam.pos));
+	norm.x = (map->tab->cam.pos.x + ray.x * sphere->t) - sphere->pos.x;
+	norm.y = (map->tab->cam.pos.y + ray.y * sphere->t) - sphere->pos.y;
+	norm.z = (map->tab->cam.pos.z + ray.z * sphere->t) - sphere->pos.z;
+	normalize_vec(&norm);
 	return (norm);
 }
 
@@ -50,6 +53,7 @@ int		sphere_lumos_spec(t_map *map, void *obj, t_vec ray, int rgb)
 	t_sphere	*s;
 
 	s = (t_sphere*)obj;
+	s->norm = get_normal_sphere(s, ray, map);
 	inter = intersection(s, ray, map->tab->cam.pos);
 	light = sous_vec(*map->tab->spot_a, inter);
 	half = add_vec(s->norm, light);
